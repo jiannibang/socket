66252d52d4533db094f783b8bfa591e7bde4ee7e
jiannibang <271381693@qq.com>
2018-07-10 12:38:52 +0800

    try catch

--- run.js
+++ run.js
@@ -1,50 +1,53 @@
 "use strict";
+try {
+  var fs = require("fs");
 
-var fs = require("fs");
+  // you'll probably load configuration from config
+  var cfg = {
+    ssl: true,
+    port: 8080,
+    ssl_key: "2_archeryscorecalculator.com.key",
+    ssl_cert: "1_archeryscorecalculator.com_bundle.crt"
+  };
 
-// you'll probably load configuration from config
-var cfg = {
-  ssl: true,
-  port: 8080,
-  ssl_key: "2_archeryscorecalculator.com.key",
-  ssl_cert: "1_archeryscorecalculator.com_bundle.crt"
-};
+  var httpServ = cfg.ssl ? require("https") : require("http");
 
-var httpServ = cfg.ssl ? require("https") : require("http");
+  var WebSocketServer = require("ws").Server;
 
-var WebSocketServer = require("ws").Server;
+  var app = null;
 
-var app = null;
+  // dummy request processing
+  var processRequest = function(req, res) {
+    res.writeHead(200);
+    res.end("All glory to WebSockets!\n");
+  };
 
-// dummy request processing
-var processRequest = function(req, res) {
-  res.writeHead(200);
-  res.end("All glory to WebSockets!\n");
-};
+  if (cfg.ssl) {
+    app = httpServ
+      .createServer(
+        {
+          // providing server with  SSL key/cert
+          key: fs.readFileSync(cfg.ssl_key),
+          cert: fs.readFileSync(cfg.ssl_cert)
+        },
+        processRequest
+      )
+      .listen(cfg.port);
+  } else {
+    app = httpServ.createServer(processRequest).listen(cfg.port);
+  }
 
-if (cfg.ssl) {
-  app = httpServ
-    .createServer(
-      {
-        // providing server with  SSL key/cert
-        key: fs.readFileSync(cfg.ssl_key),
-        cert: fs.readFileSync(cfg.ssl_cert)
-      },
-      processRequest
-    )
-    .listen(cfg.port);
-} else {
-  app = httpServ.createServer(processRequest).listen(cfg.port);
-}
-
-// passing or reference to web server so WS would knew port and SSL capabilities
-var wss = new WebSocketServer({
-  server: app
-});
-
-wss.on("connection", function(wsConnect) {
-  wsConnect.on("message", function(message) {
-    console.log(message);
-    wsConnect.send("reply");
+  // passing or reference to web server so WS would knew port and SSL capabilities
+  var wss = new WebSocketServer({
+    server: app
   });
-});
+
+  wss.on("connection", function(wsConnect) {
+    wsConnect.on("message", function(message) {
+      console.log(message);
+      wsConnect.send("reply");
+    });
+  });
+} catch (error) {
+  console.log(error);
+}
