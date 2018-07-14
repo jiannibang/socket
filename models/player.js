829e0f0690150a1ab8640c08083ea1a6ab9b1a9e
ZXH <863886245@qq.com>
2018-07-14 09:43:20 +0800

    ver 0.1 #1

--- models/player.js
+++ models/player.js
@@ -4,5 +4,20 @@ export class Player {
     this.top = (Math.ceil(Math.random() * 10) - 1) * 10;
     this.avatarUrl = avatarUrl;
     this.nickName = nickName;
+    this.HP = 100;
+    this.ATK = 1;
+    this.DEF = 0;
+    this.combatMessage = "";
+  }
+  changeAttributes(dHP, dATK, dDEF) {
+    this.HP = this.HP + dHP > 0 ? this.HP + dHP : 1;
+    this.ATK = this.ATK + dATK > 0 ? this.ATK + dATK : 1;
+    this.DEF = this.DEF + dDEF >= 0 ? this.DEF + dDEF : 0;
+  }
+  setCombatMessage(msg) {
+    this.combatMessage = msg;
+  }
+  clearCombatMessage() {
+    this.combatMessage = "";
   }
 }
